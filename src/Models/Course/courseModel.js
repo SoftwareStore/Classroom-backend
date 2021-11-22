09dda6ad65b63606f0211a35f145ad29805b7c13
andreewaD <51123966+andreewD@users.noreply.github.com>
2021-11-22 13:00:06 -0500

    courseController update
    Se genera una carpeta para archivos cuando se crea un nuevo curso

--- src/Models/Course/courseModel.js
+++ src/Models/Course/courseModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // const {User} = require('../User/userModel');
 const courseSchema = new Schema({
-    Code:String,
+    Code:{ type: String, unique: 'El codigo ({VALUE}) ya esta en uso.' },
     Name: String,
     TypeOfCourse: String,
     participants: [{ type: Schema.Types.ObjectId,ref:"user" }],
--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const passportLocal = require('passport-local').Strategy;
 const lib = require('./Test/TestData/Users');
 const app = express();
 const User = require("./Models/User/userModel");
-
+require('dotenv').config()
 //settings
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
@@ -90,7 +90,7 @@ app.get("/user", (req, res) => {
 
 //DATABASE
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://hasser:eo8ENRtR8lnASuv8@cluster0.9awkq.mongodb.net/classroom', { useNewUrlParser: true }).then(db => console.log('db is connected')).catch(err => console.log(err));
+mongoose.connect(process.env.BD_HOST, { useNewUrlParser: true }).then(db => console.log('db is connected')).catch(err => console.log(err));
 
 
 //middlewares
