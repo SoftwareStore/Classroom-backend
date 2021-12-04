9af2a406421cfa5e930e5d7cc5b5c0f69b539a85
Luis-Rojas-H <73759009+Luis-Rojas-H@users.noreply.github.com>
2021-12-04 15:45:05 -0500

    Quitando isAuthentucated

--- src/Routes/Auth-routes/auth-routes.js
+++ src/Routes/Auth-routes/auth-routes.js
@@ -4,20 +4,11 @@ const passport = require('passport')
 
 const { login, logout, google, facebook} = require('../../Controllers/Auth-routes/auth-routesController');
 
-router.get('/login', isAuthenticated, login);
-router.get('/logout', isAuthenticated, logout);
+router.get('/login', login);
+router.get('/logout', logout);
 router.get('/google', passport.authenticate('google',{scope:['email', 'profile']}));
 router.get('/facebook', passport.authenticate('facebook'));
 router.get( '/google/redirect', passport.authenticate( 'google'), google);
 router.get('/facebook/redirect', passport.authenticate('facebook'), facebook);
-
-function isAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    else{
-        res.status(400).json({ success: "no Logeado" })
-    }
-}
 
 module.exports = router;
