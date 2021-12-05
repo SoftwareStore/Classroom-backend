bda97d99f290538c915fc41e9040465877f2ec79
Luis-Rojas-H <73759009+Luis-Rojas-H@users.noreply.github.com>
2021-12-05 14:52:20 -0500

    prueba y error 4

--- src/Routes/Auth-routes/auth-routes.js
+++ src/Routes/Auth-routes/auth-routes.js
@@ -4,10 +4,13 @@ const passport = require('passport')
 
 router.get('/google', passport.authenticate('google',{scope:['profile']}));
 router.get('/facebook', passport.authenticate('facebook'));
-router.get( '/google/redirect', passport.authenticate( 'google'), GoogleFacebook);
-router.get('/facebook/redirect', passport.authenticate('facebook'), GoogleFacebook);
+router.get( '/google/redirect', passport.authenticate( 'google', {
+    successRedirect: 'http://localhost:3000/ListCurso',
+    failureRedirect: 'http://localhost:3000'
+}));
+router.get('/facebook/redirect', passport.authenticate('facebook'), Facebook);
 
-function GoogleFacebook (req, res) {
+function Facebook (req, res) {
     res.redirect('http://localhost:3000/ListCurso');
 }
 module.exports = router;
